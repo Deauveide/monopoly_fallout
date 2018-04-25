dd958e66cdd8f31905cc397920d17fcc25da04da
Deauveide <deauveide@javerianacali.edu.co>
2018-04-24 23:12:48 -0500

    Implementada base de datos con funcion de agregar usuarios

--- scripts/py/sqlite.py
+++ scripts/py/sqlite.py
@@ -4,18 +4,25 @@ con_usr = sqlite3.connect("../../db/usuarios.db")
 
 cursor = con_usr.cursor()
 
-cursor.execute(
-    "CREATE TABLE users(\
-    username VARCHAR(30) UNIQUE,\
-    password VARCHAR(30),\
-    email VARCHAR(40) UNIQUE);"
-    )
+def crearTabla():
+    cursor.execute(
+        "CREATE TABLE users(\
+        username VARCHAR(30) UNIQUE PRIMARY KEY,\
+        password VARCHAR(30),\
+        email VARCHAR(40) UNIQUE);"
+        )
+
 
 def añadirUsuario(usrName, pswd, email):
     reg = (usrName, pswd, email)
     cursor.execute("INSERT INTO users VALUES(?,?,?)", reg)
     con_usr.commit()
 
+
+def eliminarUsuario(usrName):
+    val=(usrName,)
+    cursor.execute('DELETE FROM users WHERE username=?', val)
+    con_usr.commit()
 
 
 def verDB():
